3e42247ecfbb3397915c29c17152663c697aaa84
Fuglore <44823551+fuglore@users.noreply.github.com>
2022-07-13 20:28:07 -0300

    Improved the HUD.

---
+++ hooks/managers/hud/hudchat.lua
@@ -0,0 +1,2 @@
+HUDChat.MESSAGE_FONT = tweak_data.gui.font_paths.din_compressed[24]
+HUDChat.MESSAGE_FONT_SIZE = tweak_data.gui.font_sizes.size_24
---
+++ hooks/managers/hud/hudchatmessage.lua
@@ -0,0 +1,5 @@
+HUDChatMessage.NAME_H = 26
+HUDChatMessage.NAME_FONT = tweak_data.gui.font_paths.din_compressed[26]
+HUDChatMessage.NAME_FONT_SIZE = tweak_data.gui.font_sizes.size_26
+HUDChatMessage.MESSAGE_FONT = tweak_data.gui.font_paths.din_compressed[24]
+HUDChatMessage.MESSAGE_FONT_SIZE = tweak_data.gui.font_sizes.size_24
