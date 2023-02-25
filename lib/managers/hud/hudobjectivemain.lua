badade3992c5b755fa93a0ee37125fee3a012747
Rhynne <douglaskmarx@gmail.com>
2023-02-25 02:30:51 -0700

    wagoogus

---
+++ lib/managers/hud/hudobjectivemain.lua
@@ -0,0 +1,7 @@
+HUDObjectiveMain.OBJECTIVE_TEXT_FONT = tweak_data.gui.fonts.din_compressed_outlined_32
+HUDObjectiveMain.OBJECTIVE_TEXT_FONT_SIZE = tweak_data.gui.font_sizes.size_32
+HUDObjectiveMain.AMOUNT_TEXT_FONT = tweak_data.gui.fonts.din_compressed_outlined_26
+HUDObjectiveMain.AMOUNT_TEXT_FONT_SIZE = tweak_data.gui.font_sizes.small
+HUDObjectiveMain.TIMER_SECONDS_FONT = tweak_data.gui.font_paths.din_compressed[32]
+HUDObjectiveMain.TIMER_SECONDS_FONT_SIZE = tweak_data.gui.font_sizes.size_32
+
---
+++ lib/managers/hud/hudteammatepeer.lua
@@ -0,0 +1,5 @@
+HUDTeammatePeer.PLAYER_NAME_FONT = "din_compressed_outlined_26"
+HUDTeammatePeer.PLAYER_NAME_FONT_SIZE = 26
+HUDTeammatePeer.PLAYER_LEVEL_H = 26
+HUDTeammatePeer.PLAYER_LEVEL_FONT = "din_compressed_outlined_24"
+HUDTeammatePeer.PLAYER_LEVEL_FONT_SIZE = 24
